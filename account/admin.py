b7d75c7d2a4e9e59cd3d530d15136b56e21db83d
Karttikesh Kumar <karttikesh.kumar@gmail.com>
2026-07-14 09:16:03 +0530

    Home 14-07-2026 09:15 AM

--- account/admin.py
+++ account/admin.py
@@ -1,3 +1,33 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
-# Register your models here.
+from .models import User
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    add_form = UserCreationForm
+    form = UserChangeForm
+    model = User
+
+    list_display = ('name','username','email','mobile','is_active','joined_date','last_updated')
+    search_fields = ('email','username','mobile')
+    list_filter = ('is_active','is_staff','joined_date')
+    ordering = ('joined_date',)
+
+    fieldsets = (
+        (None,               {'fields': ('username', 'password')}),
+        ('Personal info',    {'fields': ('name','mobile','email')}),
+        ('Permissions',      {'fields': ('is_active','is_staff',)}),
+        ('Groups & perms',   {'fields': ('groups','user_permissions')}),
+        ('Important dates',  {'fields': ('last_login',)}),
+    )
+
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username','mobile','email','name','password1','password2','is_active',
+                       'is_staff',)},
+        ),
+    )
+    filter_horizontal = ('groups','user_permissions')
--- account/models.py
+++ account/models.py
@@ -1,3 +1,64 @@
 from django.db import models
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser, PermissionsMixin
 
 # Create your models here.
+class UserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set.")
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email)
+        user.set_password(password)
+        user.is_active = False
+        user.save(using=self._db)
+        return user
+    
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_active', False)
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True')
+        
+        return self.create_user(email, password, **extra_fields)
+
+class UserRole(models.Model):
+    role = models.CharField(max_length=50, unique=True)
+    description = models.TextField(blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.role} - {self.description}"
+
+class User(AbstractBaseUser, PermissionsMixin):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(max_length=255, unique=True)
+    mobile = models.CharField(max_length=10, unique=True)
+    username = models.CharField(max_length=100, unique=True)
+    role = models.ManyToManyField(UserRole, related_name='UserRole')
+    is_active = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    joined_date = models.DateTimeField(auto_now_add=True)
+    last_updated = models.DateTimeField(auto_now=True)
+
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['name', 'email', 'mobile']
+
+    objects = UserManager()
+
+    def __str__(self):
+        return f"{self.name} - {self.username}"
+    
+    def has_perm(self, perm, obj=None):
+        if self.is_superuser:
+            return True
+        return super().has_perm(perm, obj)
+    
+    def has_module_perms(self, app_label):
+        if self.is_superuser:
+            return True
+        return super().has_module_perms(app_label)
