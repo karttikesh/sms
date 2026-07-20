652672fbc3c0726de67ae1e10daa72135156c189
karttikesh <karttikesh.kumar@gmail.com>
2026-07-20 18:59:18 +0530

    Office 20-07-2026 7:00 PM

--- school/admin.py
+++ school/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import School
 
 # Register your models here.
+@admin.register(School)
+class SchoolAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user', 'state', 'city', 'address']
--- school/models.py
+++ school/models.py
@@ -1,3 +1,84 @@
 from django.db import models
+from account.models import User
 
 # Create your models here.
+class School(models.Model):
+    STATE_CHOICES = (
+        ('AP', 'Andhra Pradesh'),
+        ('AP', 'Arunachal Pradesh'),
+        ('AS', 'Assam'),
+        ('BR', 'Bihar'),
+        ('CG', 'Chhattisgarh'),
+        ('GA', 'Goa'),
+        ('GJ', 'Gujarat'),
+        ('HR', 'Haryana'),
+        ('HP', 'Himachal Pradesh'),
+        ('JH', 'Jharkhand'),
+        ('KA', 'Karnataka'),
+        ('KL', 'Kerala'),
+        ('MP', 'Madhya Pradesh'),
+        ('MH', 'Maharashtra'),
+        ('MN', 'Manipur'),
+        ('ML', 'Meghalaya'),
+        ('MZ', 'Mizoram'),
+        ('NL', 'Nagaland'),
+        ('OR', 'Odisha'),
+        ('PB', 'Punjab'),
+        ('RJ', 'Rajasthan'),
+        ('SK', 'Sikkim'),
+        ('TN', 'Tamil Nadu'),
+        ('TG', 'Telangāna'),
+        ('TR', 'Tripura'),
+        ('UP', 'Uttar Pradesh'),
+        ('UK', 'Uttarakhand'),
+        ('WB', 'West Bengal')
+    )
+    CITY_CHOICES = (
+        ('AR', 'Araria'),
+        ('AW', 'Arwal'),
+        ('AU', 'Aurangabad'),
+        ('BN', 'Banka'),
+        ('BG', 'Begusarai'),
+        ('BH', 'Bhagalpur'),
+        ('BJ', 'Bhojpur'),
+        ('BX', 'Buxar'),
+        ('DB', 'Darbhanga'),
+        ('EC', 'East Champaran'),
+        ('GY', 'Gaya'),
+        ('GJ', 'Gopalganj'),
+        ('JM', 'Jamui'),
+        ('JB', 'Jehanabad'),
+        ('KM', 'Kaimur'),
+        ('KT', 'Katihar'),
+        ('KG', 'Khagaria'),
+        ('KG', 'Kishanganj'),
+        ('LS', 'Lakhisarai'),
+        ('MD', 'Madhepura'),
+        ('MB', 'Madhubani'),
+        ('MG', 'Munger'),
+        ('MZ', 'Muzaffarpur'),
+        ('NL', 'Nalanda'),
+        ('NW', 'Nawada'),
+        ('PT', 'Patna'),
+        ('PR', 'Purnia'),
+        ('RT', 'Rohtas'),
+        ('SH', 'Saharsa'),
+        ('SM', 'Samastipur'),
+        ('SH', 'Sheohar'),
+        ('SK', 'Sheikhpura'),
+        ('SR', 'Saran'),
+        ('ST', 'Sitamarhi'),
+        ('SP', 'Supaul'),
+        ('SW', 'Siwan'),
+        ('VS', 'Vaishali'),
+        ('WC', 'West Champaran')
+    )
+    name = models.CharField(max_length=200)
+    address = models.TextField()
+    state = models.CharField(max_length=100, choices=STATE_CHOICES)
+    city = models.CharField(max_length=100, choices=CITY_CHOICES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    extra_field = models.JSONField(default=dict, null=True, blank=True)
+    
+    def __str__(self):
+        return self.name
